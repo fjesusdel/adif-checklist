aae99812c67ef5e52d0b88210e29cf5ecb54ff77
fjesusdel <fjesusdel@gmail.com>
2026-06-18 08:34:23 +0200

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'adif-checklist-v1';
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(['./index.html', './manifest.json']);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      return r || fetch(e.request);
+    })
+  );
+});
